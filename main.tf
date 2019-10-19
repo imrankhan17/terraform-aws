dc47ca733494ada6ba9ddbd9b006bce552b20e9e
Imran Khan <imrankhan17@hotmail.co.uk>
2019-10-19 18:45:57 +0100

    fix key and egress issues

--- main.tf
+++ main.tf
@@ -2,25 +2,27 @@ provider "aws" {
   region = "eu-west-2"
 }
 
-resource "aws_key_pair" "tf_key" {
-  key_name = "tf_key"
-  public_key = file("~/.ssh/id_rsa.pub")
-}
-
 resource "aws_security_group" "ssh_web" {
   name = "tf_sg"
 
   ingress {
     from_port = 22
-    protocol = "tcp"
     to_port = 22
+    protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
     from_port = 80
-    protocol = "tcp"
     to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
@@ -28,21 +30,21 @@ resource "aws_security_group" "ssh_web" {
 resource "aws_instance" "web" {
   ami = "ami-0be057a22c63962cb"
   instance_type = "t2.micro"
-  key_name = aws_key_pair.tf_key.key_name
+  key_name = "my_key"
   security_groups = [aws_security_group.ssh_web.name]
 
   provisioner "remote-exec" {
-    inline = [
-      "sudo apt update -y",
-      "sudo apt install nginx -y"
-    ]
-
     connection {
       type = "ssh"
       user = "ubuntu"
       host = aws_instance.web.public_ip
       private_key = file("~/.ssh/my_key.pem")
     }
+
+    inline = [
+      "sudo apt update -y",
+      "sudo apt install nginx -y"
+    ]
   }
 
   tags = {
